664137d115170cb9d474815973c105ec4736bbbb
NinoosMoshi <nalkaton@gmail.com>
2021-08-13 18:20:41 -0400

    create Student class Closes #4

---
+++ back-end/student-management/src/main/java/com/ninos/student/model/Student.java
@@ -0,0 +1,32 @@
+package com.ninos.student.model;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Student implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String gender;
+
+    @Column(nullable = false)
+    private String customerCode;
+
+}
