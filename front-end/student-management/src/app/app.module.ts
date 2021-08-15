9370b7f8586738a83d4e21f6cdfd948ef19cc76c
NinoosMoshi <nalkaton@gmail.com>
2021-08-15 19:45:03 -0400

    Angular view and add modal Closes #10

--- front-end/student-management/src/app/app.module.ts
+++ front-end/student-management/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import {HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end/student-management/src/app/components/home/home.component.ts
+++ front-end/student-management/src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
+import { Student } from './../../model/student';
+import { StudentService } from './../../services/student.service';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  students!: Student[];
+  public viewStudent: any;
+
+  constructor(private studentService: StudentService) { }
 
   ngOnInit(): void {
+    this.getAllStudents();
   }
+
+
+
+  getAllStudents(){
+    this.studentService.getStudents().subscribe(
+      data =>{
+         this.students = data
+         console.log(data)
+      }
+    )
+  }
+
+
+
+
+  onAddStudent(addForm: NgForm){
+     document.getElementById('add-student-form')?.click();
+     this.studentService.addStudent(addForm.value).subscribe({
+       next: response =>{
+         this.getAllStudents();
+         addForm.reset();
+       },
+       error: error =>{
+        alert(error.message);
+     }
+     });
+  }
+
+
+
+  public onOpenModal(student: Student, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addStudentModal');
+    }
+
+    if (mode === 'view') {
+      this.viewStudent = student;
+      button.setAttribute('data-target', '#viewStudentModal');
+    }
+
+    container?.appendChild(button);
+    button.click();
+  }
+
+
+
 
 }
